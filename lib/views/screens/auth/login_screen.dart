9d5bf994234e311e9c03647e1a6404934e5851b8
luke choi <primera_jh@hotmail.com>
2022-03-03 15:46:22 +0900

    login screen done

--- lib/views/screens/auth/login_screen.dart
+++ lib/views/screens/auth/login_screen.dart
@@ -14,6 +14,7 @@ class LoginScreen extends StatelessWidget {
       body: Container(
           alignment: Alignment.center,
           child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
             children: [
               Text(
                 "Tiktok Clone",
@@ -30,11 +31,46 @@ class LoginScreen extends StatelessWidget {
               ),
               const SizedBox(height: 25,),
               Container(
+                width: MediaQuery.of(context).size.width,
+                margin: const EdgeInsets.symmetric(horizontal: 20),
                 child: TextInputField(
                   controller: _emailController,
                   labelText: "Email",
                   icon: Icons.email,
                 ),
+              ),
+              const SizedBox(height: 25,),
+              Container(
+                width: MediaQuery.of(context).size.width,
+                margin: const EdgeInsets.symmetric(horizontal: 20),
+                child: TextInputField(
+                  controller: _passwordController,
+                  labelText: "Password",
+                  icon: Icons.lock,
+                ),
+              ),
+              const SizedBox(height: 30,),
+              Container(
+                width: MediaQuery.of(context).size.width-40,
+                height: 50,
+                decoration: BoxDecoration(
+                  color: buttonColor,
+                  borderRadius: BorderRadius.circular(5),
+                ),
+                child: InkWell(onTap:() {
+                  print('login');
+                },child: const Center(child: Text("Login",style: TextStyle(fontSize: 20,fontWeight: FontWeight.w700),))),
+              ),
+              const SizedBox(height: 15,),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text('Don\'t have an account? ',style: TextStyle(fontSize: 20),),
+                  InkWell(onTap:(){
+                    print("navigating user");
+                  },child: Text('Register',style: TextStyle(fontSize: 20,color: buttonColor),)),
+
+                ],
               )
             ],
           )),
