1e4f2dbd4da5604b20562864f05d06aa91cd95dc
luke choi <primera_jh@hotmail.com>
2022-03-04 18:38:32 +0900

    bottom navigation bar/ homescreen/ signout function

--- lib/views/screens/home_screen.dart
+++ lib/views/screens/home_screen.dart
@@ -2,23 +2,58 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:tiktok_clone/constants.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
 
   @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  int pageIdx = 0;
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: InkWell(
-          onTap: ()=>authController.signOut(),
-          child: Container(
-            height: 100,
-            width: 100,
-            color: Colors.red,
-            child: Text('hello'),
+      bottomNavigationBar: BottomNavigationBar(
+        onTap: (idx) {
+          setState(() {
+            pageIdx = idx;
+          });
+        },
+        type: BottomNavigationBarType.fixed,
+        backgroundColor: backgroundColor,
+        selectedItemColor: Colors.red,
+        unselectedItemColor: Colors.white,
+        currentIndex: pageIdx,
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home, size: 30),
+            label: 'Home',
           ),
-        ),
-      )
+          BottomNavigationBarItem(
+            icon: Icon(Icons.search, size: 30),
+            label: 'Search',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.favorite, size: 30),
+            label: 'favorite',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.message, size: 30),
+            label: 'Messages',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person, size: 30),
+            label: 'Profile',
+          ),
+        ],
+      ),
+      body: Center(child: InkWell(onTap: ()=>authController.signOut(),child: Container(
+        height: 50,
+        width: 50,
+        color: Colors.red,
+        child: Center(child: Text("hello"),),
+      ),),),
     );
   }
 }
