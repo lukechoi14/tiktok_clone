0dce1d9f98a7353347e10b4a1edfa1cc3f3bed6f
luke choi <primera_jh@hotmail.com>
2022-03-07 13:41:17 +0900

    upload video controller

--- lib/controllers/upload_video_controller.dart
+++ lib/controllers/upload_video_controller.dart
@@ -1,6 +1,29 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:get/get.dart';
+import 'package:tiktok_clone/constants.dart';
 
 class UploadVideoController extends GetxController{
-  //upload videos
 
+  // _compressVideo(String videoPath) {
+  //   VideoCompress.compress
+  // }
+  _uploadVideoToStorage(String id, String videoPath){
+    Reference ref = firebaseStorage.ref().child('videos').child(id);
+    
+    // ref.putFile(_compressVideo(videoPath));
+  }
+  //upload videos
+  uploadVideo(String songName, String caption, String videoPath) async{
+    try{
+      String uid =firebaseAuth.currentUser!.uid;
+      DocumentSnapshot userDoc = await firestore.collection('users').doc(uid).get();
+      // get id
+      var allDocs = await firestore.collection('videos').get();
+      int len = allDocs.docs.length;
+      _uploadVideoToStroage("Video $len", videoPath);
+    }catch(e){
+
+    }
+  }
 }
