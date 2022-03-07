841f6ae3daf647109857e1e3002fc3414a9964d1
luke choi <primera_jh@hotmail.com>
2022-03-07 14:10:39 +0900

    uplaod to storage function/ compress video function

--- lib/controllers/upload_video_controller.dart
+++ lib/controllers/upload_video_controller.dart
@@ -1,29 +1,35 @@
+import 'dart:io';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:get/get.dart';
 import 'package:tiktok_clone/constants.dart';
+import 'package:video_compress/video_compress.dart';
 
-class UploadVideoController extends GetxController{
-
-  // _compressVideo(String videoPath) {
-  //   VideoCompress.compress
-  // }
-  _uploadVideoToStorage(String id, String videoPath){
-    Reference ref = firebaseStorage.ref().child('videos').child(id);
-    
-    // ref.putFile(_compressVideo(videoPath));
+class UploadVideoController extends GetxController {
+  _compressVideo(String videoPath) async {
+    final compressVideo = await VideoCompress.compressVideo(videoPath, quality: VideoQuality.MediumQuality);
+    return compressVideo!.file;
   }
+
+  Future<String> _uploadVideoToStorage(String id, String videoPath) async {
+    Reference ref = firebaseStorage.ref().child('videos').child(id);
+
+    UploadTask uploadTask = ref.putFile(await _compressVideo(videoPath));
+    TaskSnapshot snap = await uploadTask;
+    String downloadUrl = await snap.ref.getDownloadURL();
+    return downloadUrl;
+  }
+
   //upload videos
-  uploadVideo(String songName, String caption, String videoPath) async{
-    try{
-      String uid =firebaseAuth.currentUser!.uid;
-      DocumentSnapshot userDoc = await firestore.collection('users').doc(uid).get();
+  uploadVideo(String songName, String caption, String videoPath) async {
+    try {
+      String uid = firebaseAuth.currentUser!.uid;
+      DocumentSnapshot userDoc =
+          await firestore.collection('users').doc(uid).get();
       // get id
       var allDocs = await firestore.collection('videos').get();
       int len = allDocs.docs.length;
-      _uploadVideoToStroage("Video $len", videoPath);
-    }catch(e){
-
-    }
+      _uploadVideoToStorage("Video $len", videoPath);
+    } catch (e) {}
   }
 }
