958ec4615c16202ba27acf1f279bfa3d2396a7b8
luke choi <primera_jh@hotmail.com>
2022-03-07 13:02:45 +0900

    video player

--- lib/views/screens/add_video_screen.dart
+++ lib/views/screens/add_video_screen.dart
@@ -1,7 +1,10 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
-
+import 'package:get/get.dart';
 import '../../constants.dart';
+import 'confirm_screen.dart';
 
 class AddVideoScreen extends StatelessWidget {
   const AddVideoScreen({Key? key}) : super(key: key);
@@ -9,6 +12,10 @@ class AddVideoScreen extends StatelessWidget {
   pickVideo(ImageSource src, BuildContext context) async {
     final video = await ImagePicker().pickVideo(source: src);
     if (video != null) {
+      // Get.to(ConfirmScreen(
+      //   videoFile: File(video.path),
+      //   videoPath: video.path,
+      // ));
       Navigator.of(context).push(
         MaterialPageRoute(
           builder: (context) => ConfirmScreen(
