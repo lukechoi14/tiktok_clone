ea18fb0811259dcdd84cf331bcdb535d367a577d
luke choi <primera_jh@hotmail.com>
2022-03-08 16:23:18 +0900

    build profile function

--- lib/constants.dart
+++ lib/constants.dart
@@ -3,10 +3,11 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 import 'package:tiktok_clone/views/screens/add_video_screen.dart';
+import 'package:tiktok_clone/views/screens/video_screen.dart';
 import 'controllers/auth_controller.dart';
 
 List pages = [
-  Text("HomeScreen"),
+  VideoScreen(),
   Text("Search page"),
   AddVideoScreen(),
   Text("Message screen"),
--- lib/views/screens/video_screen.dart
+++ lib/views/screens/video_screen.dart
@@ -1,10 +1,98 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:tiktok_clone/views/widgets/video_player_item.dart';
+import 'package:video_player/video_player.dart';
 
 class VideoScreen extends StatelessWidget {
   const VideoScreen({Key? key}) : super(key: key);
 
+  buildProfile(String profilePhoto) {
+    return SizedBox(
+      width: 60,
+      height: 60,
+      child: Stack(children: [
+        Positioned(
+          left: 5,
+          child: Container(
+            width: 50,
+            height: 50,
+            padding: const EdgeInsets.all(1),
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(25),
+            ),
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(25),
+              child: Image(
+                image: NetworkImage(profilePhoto),
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
+        )
+      ]),
+    );
+  }
+  
   @override
   Widget build(BuildContext context) {
-    return Container();
+    final size = MediaQuery.of(context).size;
+    return Scaffold(
+      body: PageView.builder(
+          itemCount:1,
+          controller: PageController(initialPage: 0, viewportFraction: 1),
+          scrollDirection: Axis.vertical,
+          itemBuilder: (context, index) {
+            return Stack(
+              children: [
+                // VideoPlayerItem(videoUrl:,),
+                Column(
+                  children: [
+                    const SizedBox(height: 100,),
+                    Expanded(child: Row(
+                      mainAxisSize: MainAxisSize.max,
+                      crossAxisAlignment: CrossAxisAlignment.end,
+                      children: [
+                        Expanded(child: Container(
+                          padding: const EdgeInsets.only(left: 20),
+                          child: Column(
+                            mainAxisSize: MainAxisSize.min,
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            children: [
+                              Text('username', style: TextStyle(fontSize: 20,
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.bold),),
+                              Text('caption', style: TextStyle(fontSize: 15,
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.bold),),
+                              Row(
+                                children: [
+                                  Icon(Icons.music_note, size: 15, color: Colors.white,),
+                                  Text('song name', style: TextStyle(fontSize: 15,
+                                      color: Colors.white,
+                                      fontWeight: FontWeight.bold),),
+                                ],
+                              )
+                            ],
+                          ),
+                        )),
+                        Container(
+                          width: 100,
+                          margin: EdgeInsets.only(top: size.height/5),
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            children: [
+                              buildProfile('string url')
+                            ],
+                          ),
+                        )
+                      ],
+                    ))
+                  ],
+                )
+              ],
+            );
+          }),);
   }
 }
