e3998e2fcdeb1ea6b6d650ca3d501a55fb3a206a
luke choi <primera_jh@hotmail.com>
2022-03-09 13:26:52 +0900

    get comment function

--- lib/controllers/comment_controller.dart
+++ lib/controllers/comment_controller.dart
@@ -5,9 +5,10 @@ import 'package:tiktok_clone/models/comment.dart';
 import '../constants.dart';
 
 class CommentController extends GetxController {
-  final Rx<List<Comment>> _comment = Rx<List<Comment>>([]);
+  final Rx<List<Comment>> _comments = Rx<List<Comment>>([]);
 
   String _postId = "";
+  List<Comment> get comments => _comments.value;
 
   updatePostId(String id) {
     _postId = id;
@@ -15,30 +16,57 @@ class CommentController extends GetxController {
   }
 
   getComment() async {
-
+    _comments.bindStream(
+      firestore
+          .collection('videos')
+          .doc(_postId)
+          .collection('comments')
+          .snapshots()
+          .map(
+            (QuerySnapshot query) {
+          List<Comment> retValue = [];
+          for (var element in query.docs) {
+            retValue.add(Comment.fromSnap(element));
+          }
+          return retValue;
+        },
+      ),
+    );
   }
+
 
   postComment(String commentText) async {
     try {
-    if (commentText.isNotEmpty) {
-      DocumentSnapshot userDoc = await firestore.collection('users').doc(
-          authController.user.uid).get();
-      var allDocs = await firestore.collection('videos')
-          .doc(_postId)
-          .collection('comments')
-          .get();
-      int len = allDocs.docs.length;
-      
-      Comment comment = Comment(username: (userDoc.data()! as dynamic)['name'],
-          comment: commentText.trim(),
-          datePublished: DateTime.now(),
-          likes: [],
-          profilePhoto: (userDoc.data()! as dynamic)['profilePhoto'],
-          uid: authController.user.uid,
-          id: 'Comment $len');
-      await firestore.collection('videos').doc(_postId).collection('comments').doc('Comment $len').set(comment.toJson());
-    }
-    } catch(e) {
+      if (commentText.isNotEmpty) {
+        DocumentSnapshot userDoc = await firestore
+            .collection('users')
+            .doc(authController.user.uid)
+            .get();
+        var allDocs = await firestore
+            .collection('videos')
+            .doc(_postId)
+            .collection('comments')
+            .get();
+        int len = allDocs.docs.length;
+
+        Comment comment = Comment(
+            username: (userDoc.data()! as dynamic)['name'],
+            comment: commentText.trim(),
+            datePublished: DateTime.now(),
+            likes: [],
+            profilePhoto: (userDoc.data()! as dynamic)['profilePhoto'],
+            uid: authController.user.uid,
+            id: 'Comment $len');
+        await firestore
+            .collection('videos')
+            .doc(_postId)
+            .collection('comments')
+            .doc('Comment $len')
+            .set(comment.toJson());
+        DocumentSnapshot doc = await firestore.collection('videos').doc(_postId).get();
+        await firestore.collection('videos').doc(_postId).update({'commentCount':(doc.data()! as dynamic)['commentCount']+1});
+      }
+    } catch (e) {
       Get.snackbar('Error while Commenting', e.toString());
     }
   }
