cb1c41de88b6161b58d7303e81d50082c8b7b779
luke choi <primera_jh@hotmail.com>
2022-03-10 15:33:06 +0900

    search screen UI

--- lib/constants.dart
+++ lib/constants.dart
@@ -3,12 +3,13 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 import 'package:tiktok_clone/views/screens/add_video_screen.dart';
+import 'package:tiktok_clone/views/screens/search_screen.dart';
 import 'package:tiktok_clone/views/screens/video_screen.dart';
 import 'controllers/auth_controller.dart';
 
 List pages = [
   VideoScreen(),
-  Text("Search page"),
+  SearchScreen(),
   AddVideoScreen(),
   Text("Message screen"),
   Text("Profile screen"),
