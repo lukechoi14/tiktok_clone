b4b7fb3303db3c23798f936798383da18642a326
luke choi <primera_jh@hotmail.com>
2022-03-11 10:59:50 +0900

    profile screen

---
+++ lib/views/screens/profile_screen.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class ProfileScreen extends StatefulWidget {
+  final String uid;
+  const ProfileScreen({
+    Key? key,
+    required this.uid,
+  }) : super(key: key);
+
+  @override
+  _ProfileScreenState createState() => _ProfileScreenState();
+}
+
+class _ProfileScreenState extends State<ProfileScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
