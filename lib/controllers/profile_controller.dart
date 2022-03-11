502b4714959461243a2bb5ce04b196c0b3c18e07
luke choi <primera_jh@hotmail.com>
2022-03-11 11:02:07 +0900

    getuserdata function

--- lib/controllers/profile_controller.dart
+++ lib/controllers/profile_controller.dart
@@ -1,6 +1,24 @@
 import 'package:get/get.dart';
 
+import '../constants.dart';
+
 class ProfileController extends GetxController {
 
+  final Rx<Map<String, dynamic>> _user = Rx<Map<String, dynamic>>({});
+  Map<String, dynamic> get user => _user.value;
 
+  Rx<String> _uid = "".obs;
+  getUserData() async {
+    List<String> thumbnails = [];
+    var myVideos = await firestore
+        .collection('videos')
+        .where('uid', isEqualTo: _uid.value)
+        .get();
+
+    for (int i = 0; i < myVideos.docs.length; i++) {
+      thumbnails.add((myVideos.docs[i].data() as dynamic)['thumbnail']);
+    }
+
+  }
 }
+
