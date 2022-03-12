bd6c74ba9868a214da76f92ead62bec3b23e51f8
luke choi <primera_jh@hotmail.com>
2022-03-12 20:04:28 +0900

    profile controller to profile screen

--- lib/views/screens/profile_screen.dart
+++ lib/views/screens/profile_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import '../../controllers/profile_controller.dart';
 
 class ProfileScreen extends StatefulWidget {
   final String uid;
@@ -13,6 +15,13 @@ class ProfileScreen extends StatefulWidget {
 }
 
 class _ProfileScreenState extends State<ProfileScreen> {
+  final ProfileController profileController = Get.put(ProfileController());
+
+  @override
+  void initState() {
+    super.initState();
+    profileController.updateUserId(widget.uid);
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold();
